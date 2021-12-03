31302d68a295e598c162f9cb69e9acdff517ad3a
EMarcolino <estevan.marc10@gmail.com>
2021-12-03 09:23:01 -0300

    Adicionando Domains

---
+++ 2° Termo/SpMedicalGroup/2- BackEnd/senai_SpMedicalGroup_webAPI/senai_SpMedicalGroup_webAPI/Domains/Clinica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_SpMedicalGroup_webAPI.Domains
+{
+    public partial class Clinica
+    {
+        public Clinica()
+        {
+            Medicos = new HashSet<Medico>();
+        }
+
+        public int IdClinica { get; set; }
+        public int? IdEndereco { get; set; }
+        public string NomeClinica { get; set; }
+        public string Cnpj { get; set; }
+
+        public virtual Endereco IdEnderecoNavigation { get; set; }
+        public virtual ICollection<Medico> Medicos { get; set; }
+    }
+}
---
+++ 2° Termo/SpMedicalGroup/2- BackEnd/senai_SpMedicalGroup_webAPI/senai_SpMedicalGroup_webAPI/Domains/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_SpMedicalGroup_webAPI.Domains
+{
+    public partial class Paciente
+    {
+        public Paciente()
+        {
+            Consulta = new HashSet<Consultum>();
+        }
+
+        public int IdPaciente { get; set; }
+        public int? IdUsuario { get; set; }
+        public int? IdEndereco { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+
+        public virtual Endereco IdEnderecoNavigation { get; set; }
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Consultum> Consulta { get; set; }
+    }
+}
