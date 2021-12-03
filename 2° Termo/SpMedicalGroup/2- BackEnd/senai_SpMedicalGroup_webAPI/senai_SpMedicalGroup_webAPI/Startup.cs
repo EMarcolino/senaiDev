bb561d315214cad08cda672c350cbeb54442b77b
EMarcolino <estevan.marc10@gmail.com>
2021-12-03 09:27:25 -0300

    Configurando Startup

--- 2° Termo/SpMedicalGroup/2- BackEnd/senai_SpMedicalGroup_webAPI/senai_SpMedicalGroup_webAPI/Startup.cs
+++ 2° Termo/SpMedicalGroup/2- BackEnd/senai_SpMedicalGroup_webAPI/senai_SpMedicalGroup_webAPI/Startup.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai_SpMedicalGroup_webAPI
@@ -16,6 +21,60 @@ namespace senai_SpMedicalGroup_webAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //
+            services
+                .AddControllers()//
+                .AddNewtonsoftJson(options => {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+
+            //
+            services.AddCors(options =>
+                {
+                    options.AddPolicy("CorPolicy",
+                                    builder =>
+                                    {
+                                        builder.WithOrigins("http://localhost:3000")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                                    });
+                });
+
+            //
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpMedicalGroup.webAPI", Version = "v1" });
+
+                    // Set the comments path for the Swagger JSON and UI.
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
+                });
+
+            //
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "JwtBearer";
+                    options.DefaultChallengeScheme = "JwtBearer";
+                })
+
+                //
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    //
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("SpMedGroup-Autentication-Key")),
+                        ClockSkew = TimeSpan.FromMinutes(30),
+                        ValidIssuer = "SpMedicalGroup.webAPI",
+                        ValidAudience = "SpMedicalGroup.webAPI"
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +85,31 @@ namespace senai_SpMedicalGroup_webAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            //
+            app.UseSwagger();
+
+            //
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpMedicalGroup.webAPI");
+                //Define o Swagger como rota principal 
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
+            //
+            app.UseCors("CorPolicy");
+
+            //
+            app.UseAuthentication();
+
+            //
+            app.UseAuthorization();
+
+            //
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            {       //                
+                    endpoints.MapControllers();
             });
         }
     }
