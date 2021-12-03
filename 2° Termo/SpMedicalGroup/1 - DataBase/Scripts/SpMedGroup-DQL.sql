02d7e1ab39e8b33f0e279099fe0121a583c8075c
EMarcolino <estevan.marc10@gmail.com>
2021-12-03 09:16:51 -0300

    Atualizando scripts

---
+++ 2° Termo/SpMedicalGroup/1 - DataBase/Scripts/SpMedGroup-DQL.sql
@@ -0,0 +1,22 @@
+-- Definindo Banco de Dados que será usado
+USE SpMedGroup;
+GO
+
+SELECT * FROM Medico
+
+SELECT * FROM TipoUsuario
+
+SELECT * FROM Paciente
+
+SELECT * FROM Usuario
+
+SELECT * FROM Endereco
+
+SELECT * FROM Clinica
+
+SELECT * FROM Especialidade
+
+SELECT * FROM Situacao
+
+SELECT * FROM Consulta
+WHERE idPaciente = 2
